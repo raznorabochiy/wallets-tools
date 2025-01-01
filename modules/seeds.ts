4984ee3f34f2594ccac184af5236292155dba215
raznorabochiy <0xe8eabec7ce9e8bf78a766e8556e542bc2c9446ae@dmail.ai>
2025-01-01 14:03:30 +0300

    Для удобства переиспользования

--- modules/seeds.ts
+++ modules/seeds.ts
@@ -2,6 +2,13 @@ import fs from "fs";
 import { Wallet } from "ethers";
 import { SEEDS_FILENAME } from "../constants";
 
+export function generateSeed() {
+  const wallet = Wallet.createRandom();
+  const { mnemonic } = wallet;
+
+  return mnemonic!.phrase;
+}
+
 export function generateSeeds(count: number) {
   if (fs.existsSync(SEEDS_FILENAME)) {
     console.log(
@@ -13,10 +20,8 @@ export function generateSeeds(count: number) {
   const stream = fs.createWriteStream(SEEDS_FILENAME);
 
   for (let i = 0; i < count; i++) {
-    const wallet = Wallet.createRandom();
-    const { mnemonic } = wallet;
-
-    stream.write(mnemonic!.phrase + "\n");
+    const phrase = generateSeed();
+    stream.write(phrase + "\n");
   }
 
   console.log(`Сгенерированы ${count} сид-фразы в файл ${SEEDS_FILENAME}`);
